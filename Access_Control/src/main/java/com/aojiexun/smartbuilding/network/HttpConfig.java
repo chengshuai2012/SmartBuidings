f9adb936693833eca845d02ffd1065955b80a4c0
chengshuai <494886288@qq.com>
2019-07-26 14:37:43 +0800

    1

--- Access_Control/src/main/java/com/aojiexun/smartbuilding/network/HttpConfig.java
+++ Access_Control/src/main/java/com/aojiexun/smartbuilding/network/HttpConfig.java
@@ -7,7 +7,7 @@ package com.aojiexun.smartbuilding.network;
 
 public class HttpConfig {
     public static int HTTP_TIME=30;
-    public static String BASE_URL="http://120.79.50.61:8089";
+    public static String BASE_URL="http://120.79.50.61:8091";
     public static String MAC="";
     public static int id = 0;
     public static String derication = "";
